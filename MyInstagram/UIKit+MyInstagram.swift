1dbe7c71ff099edabecf786e2e520d0cc4cdc530
jinuman <jin35kim@gmail.com>
2019-03-03 15:44:46 +0900

    feat: Add anchor extension

--- MyInstagram/UIKit+MyInstagram.swift
+++ MyInstagram/UIKit+MyInstagram.swift
@@ -13,3 +13,43 @@ extension UIColor {
         self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
     }
 }
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor?,
+                leading: NSLayoutXAxisAnchor?,
+                bottom: NSLayoutYAxisAnchor?,
+                trailing: NSLayoutXAxisAnchor?,
+                marginTop: CGFloat = 0,
+                marginLeading: CGFloat = 0,
+                marginBottom: CGFloat = 0,
+                marginTrailing: CGFloat = 0,
+                width: CGFloat = 0,
+                height: CGFloat = 0) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: marginTop).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: marginLeading).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -marginBottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -marginTrailing).isActive = true
+        }
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+}
