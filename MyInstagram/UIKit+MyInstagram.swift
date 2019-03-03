84f6db01c88cb8ca93ef26b8e1f77b0f6d59e110
jinuman <jin35kim@gmail.com>
2019-03-03 14:00:10 +0900

    feat: Add UIColor extension

---
+++ MyInstagram/UIKit+MyInstagram.swift
@@ -0,0 +1,15 @@
+//
+//  UIKit+MyInstagram.swift
+//  MyInstagram
+//
+//  Created by Jinwoo Kim on 03/03/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
