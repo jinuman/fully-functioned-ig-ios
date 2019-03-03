58d68eaf8af03d1930441013cf8991c4c1062546
jinuman <jin35kim@gmail.com>
2019-03-03 12:16:56 +0900

    Start INSTAGRAM

---
+++ MyInstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyInstagram
+//
+//  Created by Jinwoo Kim on 03/03/2019.
+//  Copyright © 2019 jinuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
