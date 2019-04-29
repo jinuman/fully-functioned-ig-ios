44a9a4d96b20bd42ab2b736e6f83a26e3a35782e
jinuman <jin35kim@gmail.com>
2019-04-29 18:21:18 +0900

    feat: Add animation feedback

--- MyInstagram/Camera/PreviewPhotoContainerView.swift
+++ MyInstagram/Camera/PreviewPhotoContainerView.swift
@@ -80,6 +80,42 @@ class PreviewPhotoContainerView: UIView {
             }
             
             print("Successfully saved image to library")
+            
+            DispatchQueue.main.async {
+                let savedLabel = UILabel()
+                savedLabel.text = "Saved Successfully"
+                savedLabel.textColor = .white
+                savedLabel.textAlignment = .center
+                savedLabel.font = UIFont.boldSystemFont(ofSize: 18)
+                savedLabel.numberOfLines = 0
+                savedLabel.backgroundColor = UIColor(white: 0, alpha: 0.3)
+                
+                savedLabel.frame = CGRect(x: 0, y: 0, width: 150, height: 80)
+                savedLabel.center = self.center
+                
+                self.addSubview(savedLabel)
+                
+                savedLabel.layer.transform = CATransform3DMakeScale(0, 0, 0)
+                
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    
+                    savedLabel.layer.transform = CATransform3DMakeScale(1, 1, 1)
+                    
+                }, completion: { (completed) in
+                    
+                    UIView.animate(withDuration: 0.5, delay: 0.75, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                        
+                        savedLabel.layer.transform = CATransform3DMakeScale(0.1, 0.1, 0.1)
+                        savedLabel.alpha = 0
+                        
+                    }, completion: { (_) in
+                        
+                        savedLabel.removeFromSuperview()
+                        
+                    })
+                    
+                })
+            }
         }
     }
 }
